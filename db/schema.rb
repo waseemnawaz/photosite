db75bd3ef8bf554f3d07f1a524b9cfa3d19ef73b
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-07-20 10:02:15 +0200

    order model changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530161027) do
+ActiveRecord::Schema.define(version: 20160719111013) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "photo_id"
+    t.float   "total"
+  end
+
+  add_index "orders", ["photo_id"], name: "index_orders_on_photo_id"
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
