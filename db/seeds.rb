81b6169dab93ad93d9bc9b07d05dedf28620ad4b
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-07-20 14:57:18 +0200

    changed index in albums

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Album.create(:name => "seed-generated album", :description => "I also created this product without using the HTML form!")
